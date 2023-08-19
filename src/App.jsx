1adc7451d887eb8300e6c14c802be3c1545a6ad2
LUCIFER49 <p1ush.prj@gmail.com>
2023-08-19 14:57:02 +0530

    added settings component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import TopBar from "./components/TopBar/TopBar";
 import Home from "./Pages/Home/Home";
 import Single from "./Pages/Single/Single";
 import Write from "./Pages/Write/Write";
+import Setting from "./Pages/Settings/Settings";
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Write />
+      <Setting />
     </>
   );
 };
