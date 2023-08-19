9ec943a19cbe4b35eed531c535b6b9fb762ded22
LUCIFER49 <p1ush.prj@gmail.com>
2023-08-19 15:20:17 +0530

    added login component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Home from "./Pages/Home/Home";
 import Single from "./Pages/Single/Single";
 import Write from "./Pages/Write/Write";
 import Setting from "./Pages/Settings/Settings";
+import Login from "./Pages/Login/Login";
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Setting />
+      <Login />
     </>
   );
 };
