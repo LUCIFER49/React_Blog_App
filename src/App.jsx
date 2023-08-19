83009009327e749f52eab949b335b7a3a35a6fb1
LUCIFER49 <p1ush.prj@gmail.com>
2023-08-19 14:15:35 +0530

    added write component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import TopBar from "./components/TopBar/TopBar";
 import Home from "./Pages/Home/Home";
 import Single from "./Pages/Single/Single";
+import Write from "./Pages/Write/Write";
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
     </>
   );
 };
